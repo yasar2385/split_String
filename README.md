ac1332ab956181af4761294b09da5ba67ceb8b80
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-02-21 09:27:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# split_String
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-p4qxgc)
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var url = `https://ec.europa.eu/info/law/better-regulation/have-your-say/initiatives/1570-Evaluation-of-the-excise-duties-applied-on-manufactured-tobacco_en.`;
+
+
+var split = url.split(/\r|\n|\s/);
+
+console.log(split)
+
+if(url.split(/\r|\n|\s/).length>0){
+  console.log(true)
+}
